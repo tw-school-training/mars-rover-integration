589ad86b34d12734d3f240dd47f356add751a8d3
Zhaoyu <weixiaobo88@gmail.com>
2020-05-25 19:52:07 +0800

    [Linne] - refactor test cases to eliminate duplication

--- src/test/java/com/thoughtworks/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/MarsRoverTest.java
@@ -20,157 +20,75 @@ public class MarsRoverTest {
 
     @Test
     public void should_coordinate_y_plus_1_given_command_M_and_orientation_N() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.N);
-
-        //when
-        marsRover.execute(Command.M);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(3));
-        assertThat(marsRover.getOrientation(), is(Orientation.N));
+        checkCoordinateAndOrientation(Command.M, Orientation.N, Orientation.N, new Coordinate(1, 3));
     }
 
     @Test
     public void should_coordinate_y_minus_1_given_command_M_and_orientation_S() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.S);
-
-        //when
-        marsRover.execute(Command.M);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(1));
-        assertThat(marsRover.getOrientation(), is(Orientation.S));
+        checkCoordinateAndOrientation(Command.M, Orientation.S, Orientation.S, new Coordinate(1, 1));
     }
 
     @Test
     public void should_coordinate_x_plus_1_given_command_M_and_orientation_E() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.E);
-
-        //when
-        marsRover.execute(Command.M);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(2));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.E));
+        checkCoordinateAndOrientation(Command.M, Orientation.E, Orientation.E, new Coordinate(2, 2));
     }
 
     @Test
     public void should_coordinate_x_minus_1_given_command_M_and_orientation_W() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.W);
-
-        //when
-        marsRover.execute(Command.M);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.W));
+        checkCoordinateAndOrientation(Command.M, Orientation.W, Orientation.W, new Coordinate(0, 2));
     }
 
     @Test
     public void should_facing_W_given_command_L_and_orientation_N() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.N);
-
-        //when
-        marsRover.execute(Command.L);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.W));
+        checkCoordinateAndOrientation(Command.L, Orientation.N, Orientation.W, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_E_given_command_L_and_orientation_S() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.S);
-
-        //when
-        marsRover.execute(Command.L);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.E));
+        checkCoordinateAndOrientation(Command.L, Orientation.S, Orientation.E, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_N_given_command_L_and_orientation_E() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.E);
-
-        //when
-        marsRover.execute(Command.L);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.N));
+        checkCoordinateAndOrientation(Command.L, Orientation.E, Orientation.N, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_S_given_command_L_and_orientation_W() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.W);
-
-        //when
-        marsRover.execute(Command.L);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.S));
+        checkCoordinateAndOrientation(Command.L, Orientation.W, Orientation.S, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_E_given_command_R_and_orientation_N() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.N);
-
-        //when
-        marsRover.execute(Command.R);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.E));
+        checkCoordinateAndOrientation(Command.R, Orientation.N, Orientation.E, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_W_given_command_R_and_orientation_S() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.S);
-
-        //when
-        marsRover.execute(Command.R);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.W));
+        checkCoordinateAndOrientation(Command.R, Orientation.S, Orientation.W, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_S_given_command_R_and_orientation_E() {
-        //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.E);
-
-        //when
-        marsRover.execute(Command.R);
-        //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.S));
+        checkCoordinateAndOrientation(Command.R, Orientation.E, Orientation.S, new Coordinate(1, 2));
     }
 
     @Test
     public void should_facing_N_given_command_R_and_orientation_W() {
+        checkCoordinateAndOrientation(Command.R, Orientation.W, Orientation.N, new Coordinate(1, 2));
+    }
+
+    private void checkCoordinateAndOrientation(Command command, Orientation previousOrientation,
+                                               Orientation orientation, Coordinate coordinate) {
         //given
-        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), Orientation.W);
+        MarsRover marsRover = new MarsRover(new Coordinate(1, 2), previousOrientation);
 
         //when
-        marsRover.execute(Command.R);
+        marsRover.execute(command);
+
         //then
-        assertThat(marsRover.getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinateY(), is(2));
-        assertThat(marsRover.getOrientation(), is(Orientation.N));
+        assertThat(marsRover.getCoordinateX(), is(coordinate.getX()));
+        assertThat(marsRover.getCoordinateY(), is(coordinate.getY()));
+        assertThat(marsRover.getOrientation(), is(orientation));
     }
 }
